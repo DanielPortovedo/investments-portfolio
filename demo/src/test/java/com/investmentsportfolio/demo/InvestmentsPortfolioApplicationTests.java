c6ee78ae312e436adc96baca92567d3459e706d3
daniel_portovedo <daniel.g.portovedo@gmail.com>
2023-07-11 16:31:39 +0100

    First Commit.

---
+++ demo/src/test/java/com/investmentsportfolio/demo/InvestmentsPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investmentsportfolio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentsPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
